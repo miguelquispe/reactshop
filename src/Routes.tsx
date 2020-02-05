2e5f7026eb8d20376e51e65ee95da79c9bfbba47
Miguel Quispe <hola@miguelquispe.com>
2020-02-05 13:28:52 -0500

    Redirect and Conditional Redirect

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,20 +1,27 @@
 import * as React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import ProductsPage from './ProductsPage';
 import AdminPage from './AdminPage';
 import Header from './Header';
 import ProductPage from './ProductPage';
 import NotFoundPage from './NotFoundPage';
+import LoginPage from './LoginPage';
 
 const Routes: React.FC = () => {
+  const [loggedIn, setLoggedIn] = React.useState(false);
+  
   return (
     <Router>
       <div>
         <Header />
         <Switch>
+          <Redirect exact={true} from="/" to="/products" />
           <Route exact={true} path="/products" component={ProductsPage} />
           <Route path="/products/:id" component={ProductPage} />
-          <Route path="/admin" component={AdminPage} />
+          <Route path="/admin">
+            {loggedIn ? <AdminPage /> : <Redirect to="/login" />}  
+          </Route>
+          <Route path="/login" component={LoginPage} />
           <Route component={NotFoundPage} />
         </Switch>
       </div>
