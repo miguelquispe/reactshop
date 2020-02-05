ecfa1895670541b567c6b8a9c11c1445f2db91e7
Miguel Quispe <hola@miguelquispe.com>
2020-02-05 13:17:58 -0500

    Handling not found pages

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,18 +1,22 @@
 import * as React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import ProductsPage from './ProductsPage';
 import AdminPage from './AdminPage';
 import Header from './Header';
 import ProductPage from './ProductPage';
+import NotFoundPage from './NotFoundPage';
 
 const Routes: React.FC = () => {
   return (
     <Router>
       <div>
         <Header />
-        <Route exact={true} path="/products" component={ProductsPage} />
-        <Route path="/products/:id" component={ProductPage} />
-        <Route path="/admin" component={AdminPage} />
+        <Switch>
+          <Route exact={true} path="/products" component={ProductsPage} />
+          <Route path="/products/:id" component={ProductPage} />
+          <Route path="/admin" component={AdminPage} />
+          <Route component={NotFoundPage} />
+        </Switch>
       </div>
     </Router>
   )
